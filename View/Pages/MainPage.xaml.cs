c8f4cb697f7024701448884efc8d61f7d8b63a92
User <User@DESKTOP-S6MBTGQ>
2023-03-17 22:53:00 +0300

    Добавьте файлы проекта.

---
+++ View/Pages/MainPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿using SSD.Task._5.Core;
+using SSD.Task._5.View.Pages.TaskPages;
+using System.Windows;
+using System.Windows.Controls;
+namespace SSD.Task._5.View.Pages
+{
+    public partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
